89e29f997902fdce59c0eb7385ad801b604106ad
lealalves <leealalves@hotmail.com>
2021-09-09 22:15:54 -0300

    Feito sistema login/logout v1
    ta logando e deslogando ebaaa

---
+++ www/js/index.js
@@ -0,0 +1,65 @@
+function userInfo(){
+    $.getJSON('http://localhost/phonegap/muv/sessao.php',function(result){
+        let input = document.querySelector('#nomeusuario')
+        input.innerHTML = result.email
+    })  
+}
+
+function logout(){
+    var url = 'http://localhost/phonegap/muv/logout.php'
+    $.ajax({
+        url: url,
+        data: url,
+        success: function(data){
+            window.location.href = data;
+        }
+    })
+}
+
+function login() {
+    var email = $('#email').val();
+    var senha = $('#password').val()
+    var url = `http://localhost/phonegap/muv/login.php?email=${email}&senha=${senha}`;
+        $.ajax({
+            type: "POST",
+            crossDomain: true, 
+            cache: false,
+            url: url,
+            data: url,
+            success: function(data){              
+                if($.trim(data) == "error"){
+                    $(".inputlogin").addClass("error");
+                    console.log("n foi")                    
+                }else{
+                    userInfo();
+                    window.location.href = 'menu.html';                    
+                }                                        
+            }                       
+        });
+}
+
+function cadastrar() {
+    var email = $('#email').val();
+    var senha = $('#password').val();
+    var telefone = $('#phonenumber').val();
+    var string = `email=${email}&senha=${senha}&telefone=${telefone}&insert=`;
+
+    if ($.trim(email).length > 0 & $.trim(senha).length > 0 & $.trim(telefone).length > 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/phonegap/muv/cadastrar.php",
+            data: string,
+            crossDomain: true,
+            cache: false,
+            success: function(data) {
+                if($.trim(data) == 'error') {                            
+                        console.log("burro")
+                    }else{
+                        window.location.href = 'especificacao.html'
+                    }
+                }
+            });                
+        }else{
+            $(".inputlogin").addClass("error");
+        }        
+    }
