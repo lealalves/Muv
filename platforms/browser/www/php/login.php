cd63dbb6f17c9aea254a56043ff2dad2decbe27c
lealalves <leealalves@hotmail.com>
2021-11-19 15:54:58 -0300

    design tela finalizar corrida refeito

---
+++ platforms/browser/www/php/login.php
@@ -0,0 +1,19 @@
+<?php 
+include 'conexao.php';
+
+$email = $_REQUEST['email'];
+$senha = $_REQUEST['senha'];
+
+    $query = mysqli_query($con, "select * from usuario where email='$email' and senha='$senha'");
+
+    if(mysqli_num_rows($query) == 0){
+        echo 'error';
+    }else{
+        echo 'success';
+        $resul = mysqli_fetch_array($query);
+        if(!isset($_SESSION)) session_start();
+
+        $_SESSION['codUser'] = $resul[0];
+        $_SESSION['emailUser'] = $resul[1];
+    }
+?>
