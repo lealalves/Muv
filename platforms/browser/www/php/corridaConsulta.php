d7c4acfbb0a1e1f678069b1ef6ef9db5c319f126
lealalves <leealalves@hotmail.com>
2021-11-20 21:53:00 -0300

    aplicativo finalizado v1
    aplicativo funcionando perfeitamente
    cadastro do usuario
    criação de corrida
    finalização da corrida.

--- platforms/browser/www/php/corridaConsulta.php
+++ platforms/browser/www/php/corridaConsulta.php
@@ -5,6 +5,7 @@ include 'conexao.php';
 session_start();
 
 $query= mysqli_query($con,"select * from corrida inner join motorista on infDriver = codDriver where infDriver =" . $_SESSION['driverId']);
+
 $resul = mysqli_fetch_object($query);
 
 
